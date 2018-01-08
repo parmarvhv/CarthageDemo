3c4936b522719508be74e0874160127f0ba85289
Vaibhav Parmar <vaibhav@nickelfox.com>
2018-01-08 18:36:39 +0530

    add alamofire via carthage

---
+++ CarthageDemo/CarthageDemo.h
@@ -0,0 +1,19 @@
+//
+//  CarthageDemo.h
+//  CarthageDemo
+//
+//  Created by Vaibhav Parmar on 08/01/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageDemo.
+FOUNDATION_EXPORT double CarthageDemoVersionNumber;
+
+//! Project version string for CarthageDemo.
+FOUNDATION_EXPORT const unsigned char CarthageDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageDemo/PublicHeader.h>
+
+
