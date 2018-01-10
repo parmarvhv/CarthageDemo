c6ffe7c727ebea1365c853584acb42144709de5a
Vaibhav Parmar <vaibhav@nickelfox.com>
2018-01-10 12:46:42 +0530

    add: framework for macOS, tvOS

---
+++ CarthageDemo macOS/CarthageDemo_macOS.h
@@ -0,0 +1,19 @@
+//
+//  CarthageDemo_macOS.h
+//  CarthageDemo macOS
+//
+//  Created by Vaibhav Parmar on 10/01/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CarthageDemo_macOS.
+FOUNDATION_EXPORT double CarthageDemo_macOSVersionNumber;
+
+//! Project version string for CarthageDemo_macOS.
+FOUNDATION_EXPORT const unsigned char CarthageDemo_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageDemo_macOS/PublicHeader.h>
+
+
---
+++ CarthageDemo tvOS/CarthageDemo_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  CarthageDemo_tvOS.h
+//  CarthageDemo tvOS
+//
+//  Created by Vaibhav Parmar on 10/01/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageDemo_tvOS.
+FOUNDATION_EXPORT double CarthageDemo_tvOSVersionNumber;
+
+//! Project version string for CarthageDemo_tvOS.
+FOUNDATION_EXPORT const unsigned char CarthageDemo_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageDemo_tvOS/PublicHeader.h>
+
+
